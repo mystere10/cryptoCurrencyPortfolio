4af08a072cba6303995f34378a5371555798510e
Nkunzi Innocent <innocentnkunzi@yahoo.com>
2020-01-10 15:30:11 +0200

    connecting to the api and made search functionality

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -14,4 +14,25 @@ class HomeController < ApplicationController
   def about
     
   end
+
+  def lookup
+    require 'uri'
+    require 'net/http'
+    require 'openssl'
+
+    @url = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?CMC_PRO_API_KEY=#{ENV.fetch('CRYPTO')}&start=1&limit=500"
+    @uri = URI(@url)
+    @response = Net::HTTP.get(@uri)
+    @lookup_coins = JSON.parse(@response)
+     
+    @symbol = params[:sym]
+
+    if @symbol
+      @symbol = @symbol.upcase
+
+      if @symbol == ''
+        @symbol = 'Please provide a currency'
+      end
+    end
+  end
 end
